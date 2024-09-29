dbdf28a90b3b0e486f4154844e01a1e764ca7724
Ralph Canlas <cnlsr09@gmail.com>
2024-09-29 18:31:44 -0400

    Exec04

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Express JS');
+});
+
+app.get('/user', (req, res) => {
+  const { firstname = 'Pritesh', lastname = 'Patel' } = req.query;
+  res.json({ firstname, lastname });
+});
+
+app.post('/user/:firstname/:lastname', (req, res) => {
+  const { firstname, lastname } = req.params;
+  res.json({ firstname, lastname });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${8089}`);
+});
